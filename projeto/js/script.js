101f4a41d38455cbe002ca8d5d7e5f9d71b3c1c8
thamirislimasp <thamirislimasp@gmail.com>
2024-04-01 22:09:56 -0300

    .

--- projeto/js/script.js
+++ projeto/js/script.js
@@ -85,25 +85,7 @@
   evt.currentTarget.className += " active";
   }
 
-  // CARROSSEL DESKTOP
-
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
-    spaceBetween: 10,
-    slidesPerGroup: 2,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-
-  });
-
+  
   // MODAL
 
   // Modal
